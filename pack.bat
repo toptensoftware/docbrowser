eba7aec955d65537810a4e5d5b66e7be85f4925b
Brad Robinson <contact@toptensoftware.com>
2019-07-04 09:02:25 +1000

    Switched to less for stylesheets

--- pack.bat
+++ pack.bat
@@ -1,4 +1,6 @@
 pushd .\docbrowser\
+call lessc --source-map db-layout.less db-layout.css
+call lessc --source-map db-appearance.less db-appearance.css
 call uglifyjs db-script.js -o db-script.min.js -m --source-map "url='db-script.min.js.map'"
-call cleancss -o db-combined.min.css db-layout.css db-appearance.css --source-map
+call cleancss -o db-combined.min.css db-layout.css --input-source-map db-layout.css.map db-appearance.css --input-source-map db-appearance.css.map --source-map
 popd
